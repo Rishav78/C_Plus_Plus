5252736bfc458c9cd4894db8a7bee16a5fd7e3ca
Rishav78 <42670831+Rishav78@users.noreply.github.com>
2019-01-25 20:30:30 +0530

    Add files via upload

---
+++ avg num in array/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class parti
+{
+public:
+    parti()
+    {
+        cout << "next";
+    }
+}a;
+
+int main()
+{
+    int x = 10.2;
+    cout << x;
+    return 0;
+}
