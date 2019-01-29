ed90606d7a30b20d5803d49655efe014deb3d30b
Rishav78 <42670831+Rishav78@users.noreply.github.com>
2019-01-29 18:51:16 +0530

    Add files via upload

---
+++ frnd and member both/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class integer;
+
+class Complex
+{
+    int a,i;
+public:
+    Complex()
+    {
+        cout << "Enter complex no _ + _i"<<endl;
+        cin >> a >> i;
+    }
+    void display()
+    {
+        cout << a << "+ i" << i << endl;
+    }
+    void addition(integer y);
+};
+class integer
+{
+    int n;
+public:
+    integer()
+    {
+        cout << "Enter the Integer number:" << endl;
+        cin >> n;
+    }
+    void display()
+    {
+        cout << n;
+    }
+    friend void Complex::addition(integer y);
+};
+
+void Complex::addition(integer y)
+{
+    a = a + y.n;
+    i = i + y.n;
+}
+int main()
+{
+    Complex obj;
+    integer obj1;
+    obj.addition(obj1);
+    obj.display();
+    return 0;
+}
